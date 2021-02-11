a34882e26744ab4f211ac42e66196f2dac10eabf
Fabrice Cottin <fabrice.cottin1@viacesi.fr>
2021-02-11 09:50:43 +0100

    correction

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './classes/App';
+
 import './index.scss';
 
-const pokemon = <div>Pikachu</div>;
-
-ReactDOM.render(pokemon, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
